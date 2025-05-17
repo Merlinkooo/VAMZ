d9690304763374d5b7aa294a296ab39a2d98df9d
MakuchJakub <makuch5@stud.uniza.sk>
2025-05-17 19:07:44 +0200

    pridaný view model

--- app/src/main/java/com/example/hitmonitoring/ui/AppViewModel.kt
+++ app/src/main/java/com/example/hitmonitoring/ui/AppViewModel.kt
@@ -1,4 +1,16 @@
 package com.example.hitmonitoring.ui
 
-class AppViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.hitmonitoring.data.AppUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+.
+class AppViewModel: ViewModel() {
+
+    private val _uiState = MutableStateFlow(AppUIState())
+    val uiState: StateFlow<AppUIState> = _uiState.asStateFlow()
+
+
+
 }
