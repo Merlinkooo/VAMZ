f21dd522d5c34aca870c7fd4f92d68a3acba85c8
MakuchJakub <makuch5@stud.uniza.sk>
2025-08-18 12:20:15 +0200

    pridanie aplikačnej triedy pre prístup k dátam

---
+++ app/src/main/java/com/example/hitmonitoring/HitMonitoringApplication.kt
@@ -0,0 +1,13 @@
+package com.example.hitmonitoring
+
+import android.app.Application
+import com.example.hitmonitoring.data.AppContainer
+import com.example.hitmonitoring.data.DefaultAppContainer
+
+class HitMonitoringApplication: Application() {
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = DefaultAppContainer()
+    }
+}
